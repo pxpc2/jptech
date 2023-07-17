e10a7878148db1269010ca1f520b0ef19e32c49d
Pedro Daia <pedrodaia.c@gmail.com>
2023-07-17 12:50:46 -0300

    changing hero padding

--- client/styles/index.js
+++ client/styles/index.js
@@ -1,7 +1,7 @@
 export const styles = {
   sectionWidth: 'max-w-[1280px] w-full',
   sectionPadding: 'py-24 my-18 px-8 mx-auto',
-  heroPadding: 'py-18 pb-32 px-8 mx-auto',
+  heroPadding: 'pt-8 pb-52 px-8 mx-auto',
 
   paddingX: 'px-8 mx-auto',
 };
