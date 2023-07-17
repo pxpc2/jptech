38fa6817f2692f64966d79d101ebfaefd75da399
Pedro Daia <pedrodaia.c@gmail.com>
2023-07-17 12:14:53 -0300

    background colors and margins

--- client/app/page.js
+++ client/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="bg-hero-navbar-img bg-cover">
         <Navbar />
         <Hero />
-        <div class="custom-shape-divider-bottom-1689604045">
+        <div className="custom-shape-divider-bottom-1689604045">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -29,8 +29,21 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-slate-200 py-14">
+      <div className="bg-slate-100 py-12">
         <Marcas />
+      </div>
+      <div class="custom-shape-divider-top-1689606572">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
       </div>
       <div className="bg-white">
         <Solucoes />
--- client/styles/index.js
+++ client/styles/index.js
@@ -1,6 +1,6 @@
 export const styles = {
   sectionWidth: 'max-w-[1280px] w-full',
-  sectionPadding: 'py-32 my-18 px-8 mx-auto',
+  sectionPadding: 'py-24 my-18 px-8 mx-auto',
   heroPadding: 'py-18 pb-32 px-8 mx-auto',
 
   paddingX: 'px-8 mx-auto',
