fcf67cb542624b0e8b60d27024f184da01892c52
Pedro Daia <pedrodaia.c@gmail.com>
2023-07-19 11:27:11 -0300

    replacing image placeholder on Solucoes section, and changing texts

--- client/sections/Solucoes.jsx
+++ client/sections/Solucoes.jsx
@@ -7,7 +7,7 @@ export default function Solucoes() {
       <div className="flex flex-row flex-nowrap items-center gap-20">
         <div className="SOLUCOES__IMAGEM w-full">
           <Image
-            src={'/Triangle.png'}
+            src={'/tech-world.jpg'}
             alt="triangle"
             width={700}
             height={700}
@@ -15,10 +15,12 @@ export default function Solucoes() {
         </div>
         <div className="SOLUCOES__TEXTOS  flex flex-col gap-8">
           <div className="flex flex-row flex-nowrap w-[500px]">
-            <h1 className="text-gray-800 text-[40px] font-medium leading-[48px] w-[250px]">
+            <h1 className="text-gray-800 text-[40px] font-normal leading-[48px] w-[250px]">
               Levamos a inovação tecnológica para&nbsp;
-              <span className="underline font-bold">todo</span> o território
-              nacional.
+              <span className="bg-clip-text font-medium text-transparent bg-gradient-to-r from-[#0283C9] to-[#67a2f0]">
+                todo
+              </span>{' '}
+              o território nacional.
             </h1>
             <Image
               src={'/placeholder.svg'}
@@ -28,8 +30,8 @@ export default function Solucoes() {
             />
           </div>
 
-          <p className="text-gray-500 text-[20px] leading-[32px] w-[500px] h-[135px]">
-            Tornarmos referência em tecnologia para empresas e instituições
+          <p className="text-slate-800 font-light text-[20px] leading-[32px] w-[500px] h-[135px]">
+            Nos tornamos referência em tecnologia para empresas e instituições
             governamentais, reconhecidos por nossa excelência, inovação e
             compromisso com a satisfação do cliente.
           </p>
