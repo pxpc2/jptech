067d98311b38af4ca3714dcef2d8087e2a9be486
Pedro Daia <pedrodaia.c@gmail.com>
2023-07-24 15:28:52 -0300

    creating contact page

---
+++ client/components/ContactForm/index.jsx
@@ -0,0 +1,177 @@
+export default function ContactForm() {
+  return (
+    <div
+      className="bg-slate-50 py-16 px-4 overflow-hidden sm:px-6 
+    lg:px-8 lg:py-24"
+    >
+      <div className="relative max-w-xl mx-auto">
+        <svg
+          className="absolute left-full transform translate-x-1/2"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
+        </svg>
+        <svg
+          className="absolute right-full bottom-0 transform -translate-x-1/2"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
+        </svg>
+        <div className="text-center">
+          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Entre em contato
+          </h1>
+          <p className="mt-4 text-lg leading-6 text-gray-500">
+            Preencha o formulário com seus dados e suas dúvidas, retornaremos
+            assim que possível.
+          </p>
+        </div>
+        <div className="mt-12">
+          <form
+            action="#"
+            method="POST"
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nome
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500
+                   border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Sobrenome
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Mensagem
+              </label>
+              <div className="mt-1">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="py-3 px-4 block w-full shadow-sm
+                   focus:ring-indigo-500 focus:border-indigo-500 border
+                    border-gray-300 rounded-md"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <button
+                type="submit"
+                className="w-full inline-flex items-center justify-center
+                 px-6 py-3 border border-transparent rounded-md shadow-sm 
+                 text-base font-medium text-white bg-jptech-brand-blue
+                  hover:bg-jptech-brand-lighterblue focus:outline-none focus:ring-2 
+                  focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Enviar mensagem!
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
