26adc465392b9c79871b963a9fe30b8708471379
Pedro Daia <pedrodaia.c@gmail.com>
2023-08-04 15:12:07 -0300

    changing world image and adding new animations

--- client/sections/Features.jsx
+++ client/sections/Features.jsx
@@ -2,24 +2,30 @@ import Image from 'next/image';
 
 export default function Solucoes() {
   return (
-    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:py-12">
-      <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+    <div className="max-w-7xl mx-auto sm:px-12 lg:px-12 sm:py-12">
+      <div
+        className="relative shadow-lg hover:shadow-2xl 
+      duration-500 sm:rounded-2xl sm:overflow-hidden"
+      >
         {' '}
-        <div className="flex flex-col sm:flex-row px-12 sm:px-0 flex-nowrap items-center sm:gap-20 ">
+        <div
+          className="flex flex-col sm:flex-row px-12 sm:px-0 sm:py-24 
+        flex-nowrap items-center ml-8 sm:gap-20 "
+        >
           <div className="SOLUCOES__IMAGEM w-full">
             <Image
-              src={'/tech-world.png'}
+              src={'/world.png'}
               alt="triangle"
               width={700}
               height={700}
-              className=""
+              className="ease-in-out duration-500 sm:hover:scale-105"
             />
           </div>
           <div className="SOLUCOES__TEXTOS flex flex-col gap-8">
             <div className="flex flex-col sm:flex-row flex-nowrap sm:w-[500px]">
               <h1
                 className="text-gray-800 text-3xl sm:text-[40px] text-center
-               sm:text-left font-semibold sm:leading-[48px] sm:w-[400px]"
+               sm:text-left  font-semibold sm:leading-[48px] sm:w-[400px]"
               >
                 Levamos a inovação tecnológica para&nbsp;
                 <span
@@ -34,7 +40,7 @@ export default function Solucoes() {
 
             <p
               className="text-slate-800 font-normal text-[20px] leading-[32px] w-[400px] sm:h-[135px]
-            ease-in-out duration-500 sm:hover:scale-105 text-center pb-8 px-4 sm:pb-0 sm:text-left"
+             text-center pb-8 ease-in-out duration-500 hover:underline  sm:pb-0 sm:text-left"
             >
               Nos tornamos referência em tecnologia para empresas e instituições
               governamentais, reconhecidos por nossa excelência, inovação e
