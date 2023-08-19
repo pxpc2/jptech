8089525e54b4d718bff272089de769a0be35c28c
Pedro Daia <pedrodaia.c@gmail.com>
2023-08-19 02:19:14 -0300

    adding loading state to products page

---
+++ client/app/parceiros-produtos/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-row items-center justify-center top-1/2">
+      <h1 className="text-5xl font-medium uppercase">Carregando produtos...</h1>
+    </div>
+  );
+}
