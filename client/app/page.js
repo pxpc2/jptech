b6e2ec4aa79e965479f0b5918e264ca0e9f2a79e
Pedro Daia <pedrodaia.c@gmail.com>
2023-11-06 19:39:01 -0300

    adding lenis smoothscroll to home and about-us pages

--- client/app/page.js
+++ client/app/page.js
@@ -1,14 +1,26 @@
+'use client';
 import GradientWrapper from '@/components/wrappers/GradientWrapper';
 import CTA from '@/sections/CTA';
 import Features from '@/sections/World';
 import Hero from '@/sections/Hero';
 import Marcas from '@/sections/Logos';
 import Values from '@/sections/Values';
+import gsap from 'gsap';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
-/**
- * Desenvolvido pela PUBLISTR Marketing Digital.
- */
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className="w-full">
       <Hero />
--- client/app/quemsomos/page.js
+++ client/app/quemsomos/page.js
@@ -1,9 +1,22 @@
+'use client';
 import MissaoVisao from '@/components/MissaoVisao';
 import ValoresMissaoGrid from '@/components/ValoresMissaoGrid';
 import GradientWrapper from '@/components/wrappers/GradientWrapper';
 import QuemSomosHero from '@/sections/QuemSomos';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 export default function QuemSomos() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <div className="w-full relative">
       <QuemSomosHero />
